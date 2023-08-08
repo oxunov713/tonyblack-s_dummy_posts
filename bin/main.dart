dff5e6dba9a1c601367002fccc1876e5385fb082
oxunov_713 <aoxunov713@gmail.com>
2023-08-08 23:49:55 +0500

    get_single

--- bin/main.dart
+++ bin/main.dart
@@ -2,15 +2,16 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:main/class/post_model.dart';
+import 'package:main/class/posts.dart';
 
 Future<void> main() async {
   String baseUrl = "https://dummyjson.com";
 
-  Uri uri = Uri.parse("$baseUrl/posts");
+  Uri uri = Uri.parse("$baseUrl/posts/1");
   http.Response response = await http.get(uri);
 
   Map<String, Object?> json = jsonDecode(response.body);
 
-  PostModel? posts = PostModel.fromJson(json);
+  Posts? posts = Posts.fromJson(json);
   print(posts);
 }
